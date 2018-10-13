02d5290b9536e3865db4e29edad523b22fd140a5
Morgan Borjigin-Wang <mbwang10@gmail.com>
2018-10-12 21:39:51 -0400

    Made JSON file and deserialization methods

---
+++ Fall/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public static List<Dictionary<string, List<List<int>>>> paths;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Fall/Assets/Scripts/StartUpScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class StartUpScript : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		EnemyController.paths = JsonConvert.DeserializeObject<List<Dictionary<string, List<List<int>>>>>(File.ReadAllText(@"Assets/Scripts/paths.json"));
+		Debug.Log(EnemyController.paths[0]["0_0"][0][0]);
+		Debug.Log(EnemyController.paths[0]["0_0"][0][1]);
+		Debug.Log(EnemyController.paths[0]["0_0"][1][0]);
+		Debug.Log(EnemyController.paths[0]["0_0"][1][1]);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
